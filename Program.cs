d2ade9b95161e9fd84e53408fa961bcc7311dafd
CelineMklld <37672505+CelineMklld@users.noreply.github.com>
2019-05-05 19:46:24 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GestionClotureGSB
+{
+    class Program
+    {
+
+        /// <summary>
+        /// Méthode "main" : exécution des modifications de BDD à apporter 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            /* Tests unitaires 
+            
+            DatabaseConnection connection = new DatabaseConnection(@"Server=localhost;Port=3306;Database=gsb_frais;Uid=root;Pwd=;");
+            connection.adminRequest("UPDATE visiteur SET nom='Andreee' WHERE id='a17'"); 
+
+            Console.WriteLine(connection.getFromDb("SELECT nom FROM visiteur"));
+            Console.ReadKey();
+
+            Console.WriteLine(gestionDates.getMoisPrecedent(new DateTime(2018, 12, 25)));
+            Console.ReadKey();
+
+            Console.WriteLine(gestionDates.getMoisSuivant());
+            Console.ReadKey();
+
+            Console.WriteLine(gestionDates.getMoisSuivant(new DateTime(2018, 12, 25)));
+            Console.ReadKey();
+
+            Console.WriteLine(gestionDates.entre(15, 20));
+            Console.ReadKey();
+
+            Console.WriteLine(gestionDates.entre(new DateTime(2018, 12, 25), 15, 29));
+            Console.ReadKey();
+
+            */ 
+
+
+            // Modifications à apporter
+            DatabaseConnection connexion = new DatabaseConnection(@"Server=localhost;Port=3306;Database=gsb_frais;Uid=root;Pwd=;");
+
+            while(true)
+            {
+                string moisPrecedent = gestionDates.getMoisPrecedent();
+
+
+                if(gestionDates.entre(1, DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month)))
+                {
+                    connexion.adminRequest("UPDATE fichefrais SET idetat='CL' WHERE idetat='CR'AND (SELECT RIGHT(mois, 2)='" + moisPrecedent + "')");
+                }
+
+
+                if (gestionDates.entre(20, DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month)))
+                {
+                    string dateNow = DateTime.Now.ToString("yyyy-MM-dd");
+                    connexion.adminRequest("UPDATE fichefrais SET idetat='RB', datamodif='"+dateNow+"' WHERE idetat='VA' AND (SELECT RIGHT(mois, 2)='" + moisPrecedent + "')");
+                }
+
+                Thread.Sleep(2000);
+            }
+
+        }
+
+    }
+}
---
+++ gestionDates.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionClotureGSB
+{
+    public abstract class gestionDates
+    {
+        /// <summary>
+        /// Méthode « getMoisPrecedent » : récupère le mois précédent par rapport à la date actuelle
+        /// </summary>
+        /// <returns> Retourne le mois précédent par rapport à la date actuelle </returns>
+        public static string getMoisPrecedent()
+        {
+            return DateTime.Now.AddMonths(-1).ToString("MM");
+        }
+
+        /// <summary>
+        /// Méthode « getMoisPrecedent » : récupère le mois précédent par rapport à une date aléatoire
+        /// </summary>
+        /// <param name="date"> Date dont on veut obtenir le mois précédent </param>
+        /// <returns> Retourne le mois précédent par rapport à la date en paramètre </returns>
+        public static string getMoisPrecedent(DateTime date)
+        {
+            return date.AddMonths(-1).ToString("MM");
+        }
+
+        /// <summary>
+        /// Méthode « getMoisSuivant » : récupère le mois suivant par rapport à la date actuelle
+        /// </summary>
+        /// <returns> Retourne le mois suivant par rapport à la date actuelle </returns>
+        public static string getMoisSuivant()
+        {
+            return DateTime.Now.AddMonths(+1).ToString("MM");
+        }
+
+        /// <summary>
+        /// Méthode « getMoisSuivant » : récupère le mois suivant par rapport à une date aléatoire
+        /// </summary>
+        /// <param name="date"> Date dont on veut obtenir le mois suivant </param>
+        /// <returns> Retourne le mois suivant par rapport à la date en paramètre </returns>
+        public static string getMoisSuivant(DateTime date)
+        {
+            return date.AddMonths(+1).ToString("MM");
+        }
+
+        /// <summary>
+        /// Méthode booléenne « entre » : avec jour actuel entre 2 dates 
+        /// </summary>
+        /// <param name="jourUn"> Premier jour de la fourchette de dates </param>
+        /// <param name="jourDeux"> Dernier jour de la fourchette de dates </param>
+        /// <returns> Retourne le jour actuel entre les 2 jours en paramètre </returns>
+        public static bool entre(int jourUn, int jourDeux)
+        {
+            int jourActuel = DateTime.Now.Day;
+            return jourActuel <= jourDeux && jourActuel >= jourUn;
+            
+        }
+
+        /// <summary>
+        /// Méthode booléenne « entre » : avec jour aléatoire entre 2 dates
+        /// </summary>
+        /// <param name="date"> Date comprise entre la fourchette des 2 jours </param>
+        /// <param name="jourUn"> Premier jour de la fourchette de dates </param>
+        /// <param name="jourDeux"> Dernier jour de la fourchette de dates </param>
+        /// <returns> Retourne un jour entre les 2 jours en paramètre </returns> 
+        public static bool entre(DateTime date, int jourUn, int jourDeux)
+        {
+            int jourTest = date.Day;
+            return jourTest <= jourDeux && jourTest >= jourUn;
+        }
+
+    }
+}
